19d2c892f3739f1c8f498d9d41dff532dee85524
Lacrizomiq <lufo.rn84@gmail.com>
2023-03-31 15:07:37 +0200

    Update vanDetail.jsx

--- src/pages/vanDetail.jsx
+++ src/pages/vanDetail.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
-    const [van, setVan] = useState([])
+    const [van, setVan] = useState(null)
     const params = useParams()
 
     useEffect(() => {
