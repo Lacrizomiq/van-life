545dc1ca9dcede7720ee407bbb8840a96fdc2d46
Lacrizomiq <lufo.rn84@gmail.com>
2023-04-04 08:52:28 +0200

    header fixing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useLocation, NavLink } from "react-router-dom";
 
 export default function Header() {
+
+    const location = useLocation()
+
     return (
     <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
+        <NavLink className="site-logo" to="/" isActive={() => location.pathname === '/'}>#VanLife</NavLink>
         <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
+            <NavLink to="/about" isActive={() => location.pathname === '/about'}>About</NavLink>
+            <NavLink to="/vans" isActive={() => location.pathname === '/vans'}>Vans</NavLink>
         </nav>
     </header>
     )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Header from "./Header";
 
 function Layout() {
     return(
         <>
-            <p>Yo</p>
+            <Header />
             <Outlet />
         </>
         
