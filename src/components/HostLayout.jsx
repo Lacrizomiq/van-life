5185d53ec003abeb491838b29e30a2b3fd809735
Lacrizomiq <lufo.rn84@gmail.com>
2023-04-06 13:42:43 +0200

    Update HostLayout.jsx

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -5,7 +5,7 @@ function HostLayout() {
     return (
         <>
             <nav className="host-nav">
-                <Link to='/host/dashboard'>Dashboard</Link>
+                <Link to='/host'>Dashboard</Link>
                 <Link to='/host/income'>Income</Link>
                 <Link to='/host/reviews'>Reviews</Link>
             </nav>
