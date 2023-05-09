482dfb13704c93aaedfbe31a36011e9e7fd7387f
Lacrizomiq <lufo.rn84@gmail.com>
2023-05-09 15:23:31 +0200

    Update Vans.jsx
    starting to implement useSearchParams

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 
 function Vans(props) {
 
     const [vans, setVans] = useState([])
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    const typeFilter = searchParams.get('type')
+    console.log(typeFilter)
 
     useEffect(() => {
         fetch("/api/vans")
